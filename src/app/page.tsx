307e5654ec0ec1a24d15e9351682cbe59608f000
Archit <architrathod77@gmail.com>
2023-03-04 14:27:22 +0530

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 "use client";
 import React, { useState, useEffect } from "react";
 
+import Navbar from "../../components/Landing/Navbar";
+import Hero from "../../components/Landing/Hero";
+import CardAnimation from "components/Landing/CardAnimation";
+import Features from "components/Landing/Features";
+import Testimonials from "components/Landing/Testimonials";
+import Footer from "components/Landing/Footer";
+
 type Props = {};
 
 const Page = (props: Props) => {
-  return <div className=""></div>;
+    return (
+        <div className="">
+            <Navbar />
+            <Hero />
+            {/* <CardAnimation /> */}
+            <Features />
+            <Testimonials />
+            <Footer />
+        </div>
+    );
 };
 
 export default Page;
